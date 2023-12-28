633c84e78c87417761c172bdff0814078bac0b10
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-12-28 23:26:22 +0000

    0. Hello Flask!

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python3
 
+"""
+A script that starts a Flask web applicatio
+"""
 from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def home():
+    """
+    returnig "Hello HBNB!" at 0.0.0.0:5000/
+    """
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
-
