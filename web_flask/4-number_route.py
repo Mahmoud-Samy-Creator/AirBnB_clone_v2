aa37974f141bbe62588d7afe5b8cc22a05fc5960
Mahmoud-Samy-Creator <mahmoud.samy.elshora@gmail.com>
2023-12-29 00:09:44 +0000

    4. Is it a number?

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web applicatio
+"""
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """
+    display "Hello HBNB!" at 0.0.0.0:5000/
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    display “HBNB” at 0.0.0.0:5000/
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """
+    display: “C ” followed by the value of the text variable
+    (replace underscore _ symbols with a space )
+    """
+    return f"C {escape(text.replace('_', ' '))}"
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    """
+    display: “python ” followed by the value of the text variable
+    (replace underscore _ symbols with a space )
+    """
+    return f"Python {escape(text.replace('_', ' '))}"
+
+
+@app.route("/number/<n>", strict_slashes=False)
+def n_is_num(n):
+    """
+    display “n is a number” only if n is an integer
+    """
+    number = float(n)
+    if number.is_integer():
+        return f"{escape(int(number))} is a number"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
